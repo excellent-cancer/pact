b89b81988ecbeb0da9d1180eced72efc5c28a150
XyParaCrim <xylome@qq.com>
2020-04-12 02:40:14 +0800

    feat(note): modify and move previously completed annotations
    1.add @OnDevelopment to mark efficient and planned annotations
    2.move @Unqualified and delete other old

---
+++ perishing-constraint-note/src/main/java/perishing/constraint/note/OnDevelopment.java
@@ -0,0 +1,16 @@
+package perishing.constraint.note;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 标记帮助计划发开的注解
+ *
+ * @author XyParaCrim
+ */
+@Target(ElementType.ANNOTATION_TYPE)
+@Retention(RetentionPolicy.SOURCE)
+@interface OnDevelopment {
+}
