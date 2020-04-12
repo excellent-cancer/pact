cbd37698a05529fb205b5b26c48ef6a1e486a036
XyParaCrim <xylome@qq.com>
2020-04-12 23:06:12 +0800

    feat(web): create new module: perishing-constraint-web
    1.new module(perishing-constraint-web) for standardize commonly used functions in wen applications
    2.fixed version confusion between modules

--- perishing-constraint-treasure-chest/src/main/java/perishing/constraint/treasure/chest/CollectionsTreasureChest.java
+++ perishing-constraint-treasure-chest/src/main/java/perishing/constraint/treasure/chest/CollectionsTreasureChest.java
@@ -4,7 +4,10 @@ import perishing.constraint.note.PatternRemark;
 
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 /**
  * 提供对于集合的常用功能
@@ -29,6 +32,19 @@ public final class CollectionsTreasureChest {
             return set;
         }
         return Collections.emptySet();
+    }
+
+    /**
+     * 将{@link Iterable<T>}类型转成{@link List <T>}
+     *
+     * @param elements 可迭代元素
+     * @param <T>      元素类型
+     * @return 返回一个List
+     */
+    public static <T> List<T> asList(Iterable<? extends T> elements) {
+        return elements == null ?
+                Collections.emptyList() :
+                StreamSupport.stream(elements.spliterator(), false).collect(Collectors.toList());
     }
 
 }
---
+++ perishing-constraint-web/src/main/java/perishing/constraint/web/ResponseCode.java
@@ -0,0 +1,42 @@
+package perishing.constraint.web;
+
+/**
+ * 服务器返回客户端的返回码
+ *
+ * @author XyParaCrim
+ */
+public interface ResponseCode {
+
+    /**
+     * @return 返回返回码的值
+     */
+    int value();
+
+    /**
+     * 定义一些通用的返回码
+     */
+    enum CommonResponseCode implements ResponseCode {
+
+        /**
+         * 成功状态
+         */
+        SUCCESS(0),
+
+        /**
+         * 错误状态
+         */
+        ERROR(-1);
+
+        private final int value;
+
+        CommonResponseCode(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public int value() {
+            return value;
+        }
+    }
+
+}
